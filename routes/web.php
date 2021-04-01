7332c67e44e66fa03d50c7048bdad01c44c5db1b
Fanny581 <fannyyulissacruz16@gmail.com>
2021-03-31 19:40:06 -0600

    enlazar controladoe

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+    Route::get('/ruta1', function () {
+        return ('Ruta 1');
+    });
+Route::get('/ruta2', function () {
+    return ('Ruta 2');
+});
+    Route::get('/ruta3', function () {
+        return ('ruta 3');
+    });
